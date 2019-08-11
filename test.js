ed8b125a7ca99fd168cd6a45108345c313383d63
Adam Weidner <adam@geome.company>
2019-08-11 14:09:21 -0400

    Adds characterization tests

--- test.js
+++ test.js
@@ -1,3 +1,20 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+
+QUnit.module("Characterization tests")
+
+QUnit.test("5k stay yields a P(Win) of around .33", function( assert ) {
+    for (var i = 0; i < 5000; i++) {
+        pickDoor("door" + getRandomInt(0, 3));
+        stay();
+    }
+    assert.ok((Math.abs(game.stayed.won / 5000 - .33)) < .01)
 });
+
+QUnit.test("5k switch yields a P(Win) of around .66", function( assert ) {
+    for (var i = 0; i < 5000; i++) {
+        pickDoor("door" + getRandomInt(0, 3));
+        switchDoor();
+    }
+    assert.ok((Math.abs(game.switched.won / 5000 - .66)) < .01)
+});
+
+
